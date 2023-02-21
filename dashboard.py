9cf3554d5a35a3e7f26e6425ce6c8c2d8cb7ab8d
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2023-02-21 12:37:28 +0700

    sdfsdf

--- dashboard.py
+++ dashboard.py
@@ -23,19 +23,19 @@ from flask_breadcrumbs import Breadcrumbs, register_breadcrumb
 
 
 # Kết nối database sql server
-def get_db():
-    server = '192.168.123.254'
-    database = 'eHospital_NgheAn'
-    username = 'sa'
-    password = 'toanthang'
-    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
-                          server+';DATABASE='+database+';UID='+username+';PWD=' + password)
-    return cnxn
-
 # def get_db():
-#     cnxn = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}', server='localhost', database='eHospital_NgheAn',               
-#                trusted_connection='yes')
+#     server = '192.168.123.254'
+#     database = 'eHospital_NgheAn'
+#     username = 'sa'
+#     password = 'toanthang'
+#     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
+#                           server+';DATABASE='+database+';UID='+username+';PWD=' + password)
 #     return cnxn
+
+def get_db():
+    cnxn = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}', server='localhost', database='eHospital_NgheAn',               
+               trusted_connection='yes')
+    return cnxn
 
 def get_change(current, previous):
     if not current:
@@ -630,7 +630,7 @@ def confirmed(day_query=None):
     today = day_dict['today']
 
     all_confirmed = query_confirmed.list(today, cursor)
-    all_confirmed = ([thoigian.strftime("%H:%M:%S %d-%m-%Y"),soxacnhan,benhnhan_id,  f'{int(doanhthu):,}', int(thanhtoan), phongkham,nhanvien] for thoigian,soxacnhan, benhnhan_id, doanhthu, thanhtoan,phongkham, nhanvien in all_confirmed)
+    all_confirmed = ([thoigian.strftime("%H:%M:%S %d-%m-%Y"),soxacnhan,benhnhan_id,  f'{int(doanhthu):,}', f'{int(thanhtoan):,}', phongkham,nhanvien] for thoigian,soxacnhan, benhnhan_id, doanhthu, thanhtoan,phongkham, nhanvien in all_confirmed)
 
     table_column_title = ['Thời gian', 'Số xác nhận', 'ID Bệnh nhân', 'Doanh thu', 'Thanh toán', 'Phòng khám', 'Nhân viên']
 
