32c98626645ec548c3f6da0641c49c94ec0490e9
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2023-03-27 12:29:57 +0700

    fgg

--- testquery.py
+++ testquery.py
@@ -6,8 +6,10 @@ server = '192.168.123.254'
 database = 'eHospital_NgheAn'
 username = 'dashboard'
 password = 'ttytanhson@2023'
-cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER=' +
-                    server+';DATABASE='+database+';UID='+username+';PWD=' + password)
+cnxn = pyodbc.connect(driver='{ODBC Driver 17 for SQL Server}', server='localhost', database='eHospital_NgheAn',
+                                trusted_connection='yes')
+# cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER=' +
+#                     server+';DATABASE='+database+';UID='+username+';PWD=' + password)
 cursor = cnxn.cursor()
 
 class bcolors:
@@ -23,11 +25,25 @@ class bcolors:
 
 def test():
     query = """
-    SELECT
-    SUM(t1.SoLuong*t1.DonGiaDoanhThu) as total
-    FROM XacNhanChiPhiChiTiet as t1
-    INNER JOIN XacNhanChiPhi as t2
-    ON t1.XacNhanChiPhi_Id=t2.XacNhanChiPhi_Id AND (t2.ThoiGianXacNhan BETWEEN '2022-01-01' AND '2023-01-01')
+    SELECT SUM(t2.SoLuong * t2.DonGiaDoanhThu)
+    FROM
+    (SELECT XacNhanChiPhi_Id FROM XacNhanChiPhi WHERE XacNhanChiPhi.ThoiGianXacNhan BETWEEN '2022-01-01' AND '2023-01-01') as t1
+    INNER JOIN XacNhanChiPhiChiTiet as t2
+    ON t1.XacNhanChiPhi_Id = t2.XacNhanChiPhi_Id
+    """
+    try:
+        q = cursor.execute(query).fetchall()
+        return q
+    except:
+        print("Lỗi query phannhom_money")
+        return 0    
+def test2():
+    query = """
+    SELECT SUM(SoLuong * DonGiaDoanhThu)
+    FROM XacNhanChiPhi
+    INNER JOIN XacNhanChiPhiChiTiet
+    ON XacNhanChiPhi.XacNhanChiPhi_Id = XacNhanChiPhiChiTiet.XacNhanChiPhi_Id
+    WHERE XacNhanChiPhi.ThoiGianXacNhan BETWEEN '2022-01-01' AND '2023-01-01'
     """
     try:
         q = cursor.execute(query).fetchall()
@@ -42,6 +58,22 @@ max = 100000
 for i in range(10):
     start_time = time.time()
     t = test()
+    q_time = time.time() - start_time
+    if min < q_time:
+        min = q_time
+    if max > q_time:
+        max = q_time
+
+    total += q_time
+
+print(f'{bcolors.OKGREEN}min q time: {max}; {bcolors.FAIL}max q time {min}; {bcolors.OKBLUE}average time: {total/10}')
+
+total = 0
+min = -100000
+max = 100000
+for i in range(10):
+    start_time = time.time()
+    t = test2()
     q_time = time.time() - start_time
     if min < q_time:
         min = q_time
